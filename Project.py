84348059331acd11ba043660714e84fea2070586
shassan4162 <39094351+shassan4162@users.noreply.github.com>
2019-08-07 21:26:50 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -4,6 +4,9 @@ import requests
 import os
 import urllib.request
 from bs4 import BeautifulSoup
+os.mkdir('All Data') 
+import shutil
+import fnmatch
 
 
 # Set the URL you want to webscrape from
@@ -22,18 +25,63 @@ for i in range(36,len(soup.findAll('a'))+1): #'a' tags are for links
     download_url = 'http://web.mta.info/developers/'+ link
     urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')+1:]) 
 
-os.makedirs('2019/01')
-os.makedirs('2019/02')
-os.makedirs('2019/03')
-os.makedirs('2019/04')
-os.makedirs('2019/05')
-os.makedirs('2019/06')
-os.makedirs('2019/07')
-os.makedirs('2019/08')
-os.makedirs('2019/09')
-os.makedirs('2019/10')
-os.makedirs('2019/11')
-os.makedirs('2019/12')
+src = 'turnstile_190803.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190727.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190720.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190713.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190706.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190629.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190622.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190615.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190608.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190601.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190525.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190518.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190511.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+for filename in os.listdir('All Data'):
+    if fnmatch.fnmatch(filename, 'turnstile_1908**.txt'):
+        print(filename)
+
+
 
 
 
@@ -46,6 +94,8 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup
 import os
+import shutil
+import fnmatch
 
 # Set the URL you want to webscrape from
 url = 'http://web.mta.info/developers/turnstile.html'
@@ -63,30 +113,66 @@ for i in range(36,len(soup.findAll('a'))-1): #'a' tags are for links
     download_url = 'http://web.mta.info/developers/'+ link
     urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')-1:]) 
 
-# do not know request number yet
-request_number = ''
-
-# base dir
-_dir = "C:\Python Project"       
-
-# create dynamic name, like "C:\Python Project\File82673"
-_dir = os.path.join(_dir, 'File%s' % request_number)
 
 # create 'dynamic' dir, if it does not exist
-if not os.path.exists(_dir):
-    os.makedirs(_dir)
+if not os.path.exists('All Data'):
+    os.makedirs('All Data')
 
-os.makedirs('2019/01')
-os.makedirs('2019/02')
-os.makedirs('2019/03')
-os.makedirs('2019/04')
-os.makedirs('2019/05')
-os.makedirs('2019/06')
-os.makedirs('2019/07')
-os.makedirs('2019/08')
-os.makedirs('2019/09')
-os.makedirs('2019/10')
-os.makedirs('2019/11')
-os.makedirs('2019/12')
+#Copying file to 'All Data" Directory
+src = 'turnstile_190803.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
 
+src = 'turnstile_190727.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190720.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190713.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190706.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190629.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190622.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190615.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190608.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190601.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190525.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190518.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+src = 'turnstile_190511.txt'
+dst = 'C:/Users/user/Desktop/Python/All Data'
+shutil.copy(src, dst)
+
+#Creating sub folder in Directory 'All Data'
+for filename in os.listdir('All Data'):
+    if fnmatch.fnmatch(filename, 'turnstile_1908**.txt'):
+        print(filename)
 
