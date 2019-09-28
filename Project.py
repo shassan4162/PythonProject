1544f5f351551a9b854575c78c862b26278f9f7d
shassan4162 <39094351+shassan4162@users.noreply.github.com>
2019-09-27 21:40:49 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,178 +1,44 @@
-# Download All Data
-# Import libraries
-import requests 
-import os
-import urllib.request
-from bs4 import BeautifulSoup
-os.mkdir('All Data') 
-import shutil
-import fnmatch
-
-
-# Set the URL you want to webscrape from
-url = 'http://web.mta.info/developers/turnstile.html'
-
-# Connect to the URL
-response = requests.get(url)
-
-# Parse HTML and save to BeautifulSoup object¶
-soup = BeautifulSoup(response.text, "html.parser")
-
-# To download the whole data set, let's do a for loop through all a tags
-for i in range(36,len(soup.findAll('a'))+1): #'a' tags are for links
-    one_a_tag = soup.findAll('a')[i]
-    link = one_a_tag['href']
-    download_url = 'http://web.mta.info/developers/'+ link
-    urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')+1:]) 
-
-src = 'turnstile_190803.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190727.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190720.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190713.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190706.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190629.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190622.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190615.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190608.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190601.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190525.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190518.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190511.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-for filename in os.listdir('All Data'):
-    if fnmatch.fnmatch(filename, 'turnstile_1908**.txt'):
-        print(filename)
-
-
-
-
-
-
-
-
-# ---- Yearly Folder Script with Opposite Pattern----
-# Import libraries
-import requests 
-import urllib.request
+import requests as re
 from bs4 import BeautifulSoup
 import os
-import shutil
-import fnmatch
 
-# Set the URL you want to webscrape from
-url = 'http://web.mta.info/developers/turnstile.html'
+def checkCreatePath(directory):
+    #it will only create a directory if its not available
+    if not os.path.exists(directory):
+        #function responsible 
+        os.makedirs(directory)
+        print("[>]"+directory+"Folder Created")
 
-# Connect to the URL
-response = requests.get(url)
+       
+#create sub directories 
+def create_sub_directories(year,month,date):
+    checkCreatePath(str(year))
+    checkCreatePath(str(year)+"/"+str(month))
+    checkCreatePath(str(year)+"/"+str(month)+"/"+str(date))
 
-# Parse HTML and save to BeautifulSoup object¶
-soup = BeautifulSoup(response.text, "html.parser")
+Page=re.get("http://web.mta.info/developers/turnstile.html")
+parser = BeautifulSoup(Page.content, 'html.parser')            
+content=parser.find('div',class_='span-84 last')
 
-# To download only 2019 data
-for i in range(36,len(soup.findAll('a'))-1): #'a' tags are for links
-    one_a_tag = soup.findAll('a')[i]
-    link = one_a_tag['href']
-    download_url = 'http://web.mta.info/developers/'+ link
-    urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')-1:]) 
+files=content.findAll('a')
 
+#loop to find all tags
+for i in files:
 
-# create 'dynamic' dir, if it does not exist
-if not os.path.exists('All Data'):
-    os.makedirs('All Data')
+	print(i['href'].ljust(40)+"   "+i.text)
+	year=i.text.split(',')[2].strip()
+	month,day=i.text.split(',')[1].split()
+	directory=year+"/"+month+"/"+day+"/"
+	create_sub_directories(year,month,day)
 
-#Copying file to 'All Data" Directory
-src = 'turnstile_190803.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
+	url=i['href']
+	file_name=url.split('/')[-1]
+	link="http://web.mta.info/developers/"+url
 
-src = 'turnstile_190727.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190720.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190713.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190706.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190629.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190622.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190615.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190608.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190601.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190525.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190518.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-src = 'turnstile_190511.txt'
-dst = 'C:/Users/user/Desktop/Python/All Data'
-shutil.copy(src, dst)
-
-#Creating sub folder in Directory 'All Data'
-for filename in os.listdir('All Data'):
-    if fnmatch.fnmatch(filename, 'turnstile_1908**.txt'):
-        print(filename)
-
+	content=re.get(link).text
+    #print(directory+file_name)
+	open(directory+file_name,'w',encoding='utf-8').write(content)
+    
+#Visual Studio code was the code editor, solution is written in python 3.2
+#To see the created changes see the directory path to where Visual Studio Code is linked to, for example for me it was in desktop folder
+#in a folder named 'python' 
